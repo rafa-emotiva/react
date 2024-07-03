bf531f839ad4f2389a3b0c6ed283dfd5e4a8e85b
Rafa-emotiva <rafael.sanchez@emotiva.net>
2024-07-03 18:26:14 +0200

    A casa

---
+++ 00-hola-mundo/src/App.jsx
@@ -0,0 +1,20 @@
+export function App () {
+
+    return(
+        <article style={{display: 'flex', alignItems: 'center', color: '#fff'}}>
+            <header>
+                <img alt="El avatar" src="https://unavatar.io/banner.png"/>
+                <div>
+                    <strong>Rafa</strong>
+                    <span>@rafa</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+
+}
