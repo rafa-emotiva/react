c5769a37761ba3eb1abcbb8b410ca64185fccf62
Rafa-emotiva <rafael.sanchez@emotiva.net>
2024-07-05 13:08:04 +0200

    Funcionando minuto 1:31:57

--- 03-mouse-follower/src/App.jsx
+++ 03-mouse-follower/src/App.jsx
@@ -1,8 +1,47 @@
 import React from 'react';
+import { useEffect, useState} from 'react';
 
 function App() {
+  const [enabled, setEnable] = useState(false)
+  const [position, setPosition] = useState({x : 0, y : 0})
+
+  useEffect(() =>{
+    // console.log('effect', {enabled})
+    const handleMove = (event) => {
+      const { clientX, clientY } = event 
+      console.log('handleMove', {clientX, clientY})
+      setPosition({x : clientX, y : clientY})
+    }
+    if(enabled){
+    window.addEventListener('pointermove', handleMove)
+    }
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
   return (
-    <h3>Proyecto 3</h3>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '3px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}/>
+
+      
+      <button onClick={() => setEnable(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </main>
+    
   )
 }
 
