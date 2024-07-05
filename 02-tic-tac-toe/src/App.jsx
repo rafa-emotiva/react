be75d9a002982d80e91be740e32b0a0942730ed7
Rafa-emotiva <rafael.sanchez@emotiva.net>
2024-07-05 10:27:25 +0200

    eslintConfig

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import './App.css'
 import confetti from "canvas-confetti"
 import { Square } from './components/Square.jsx'
@@ -49,6 +49,11 @@ function App() {
       setWinner(false) //empate
     }
   }
+
+useEffect(() =>{
+  console.log('useEffect')
+})
+
 
   return (
     <main className='board'>
