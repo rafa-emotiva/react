85e8a47add7e5df88a42919ed7ee5f201aae596a
RAFAEL SANCHEZ NAVARRO <Rafael.Sanchez14@alu.uclm.es>
2024-07-09 00:37:10 +0200

    1:20:35 funcionando

--- 05-react-buscador-peliculas/src/App.jsx
+++ 05-react-buscador-peliculas/src/App.jsx
@@ -1,20 +1,70 @@
 import './App.css'
-
-import responseMovies from "./mocks/with-results.json"
-import withoutResults from "./mocks/no-results.json"
 import { Movies } from './components/Movies'
+import { useMovies } from './hooks/useMovies'
+import { useState, useEffect, useRef } from 'react'
+
+
+function useSearch () {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
+
+    if (search === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una película con un número')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }, [search])
+
+  return { search, updateSearch, error }
+}
 
 function App() {
-  const movies = responseMovies.Search
+
+  const { search, updateSearch, error } = useSearch()
+  const {movies, getMovies} = useMovies({search})
+
+
+  
+  const handleChange = (event) => {
+    updateSearch(event.target.value)
+  }
+
+  const handleSubmit= (event) => {
+    event.preventDefault()
+    getMovies()
+  }
 
   return (
     <div className='page'>
-      <header clas>
+      <header>
         <h1>Buscador de peliculas</h1>
-        <form className='form'>
-          <input placeholder="Avangers, Stars Wars, The matrix"/>
-          <button type='submit'>Buscar</button>
+        <form className='form' onSubmit={handleSubmit}>
+        <input
+            style={{
+              border: '1px solid transparent',
+              borderColor: error ? 'red' : 'transparent'
+            }} onChange={handleChange} value={search} name='query' placeholder='Avengers, Star Wars, The Matrix...'
+          />          <button type='submit'>Buscar</button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
 
       <main>
---
+++ 05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -0,0 +1,16 @@
+import { useState } from "react"
+import { searchMovies } from "../services/movies.js"
+
+export function useMovies ({search}) {
+
+  const [movies, setMovies] = useState([])
+
+
+    const getMovies = async () => {
+      const newMovies = await searchMovies({ search })
+      setMovies(newMovies)
+    }
+
+    return {movies, getMovies}
+  
+  }
